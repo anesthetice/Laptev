b93bb4fb36b537514b2393ee236e5ec040d7fe33
anesthetice <elliot.currat@epfl.ch>
2023-02-13 20:29:31 +0100

    massive facedetection glowup

--- src/facedetection.rs
+++ src/facedetection.rs
@@ -2,53 +2,87 @@
 
 use std::time::{Instant, Duration};
 use opencv::core::{Mat, Vector, Rect, Size, Scalar};
+use opencv::gapi::sqrt;
 use opencv::imgproc::{cvt_color, COLOR_BGR2RGB, rectangle, LINE_8};
 use opencv::imgcodecs::{imread, imwrite, IMREAD_GRAYSCALE, IMREAD_COLOR};
 use opencv::highgui::{imshow, wait_key, destroy_all_windows};
 use opencv::objdetect::CascadeClassifier;
 use opencv::prelude::CascadeClassifierTrait;
 
+const LBP_CLASSIFIER_FILEPATH : &'static str = "lbpcascade_frontalface_improved.xml";
 
-// currently testing and benchmarking
-pub fn find_all_faces(image_filepath : &str, classifier_filepath : &str) -> Vector<Rect> {
+pub fn find_all_faces(image_filepath : &str, classifier_filepath : Option<&str>, verbose : bool) -> Result<Vector<Rect>, ()> {
+
+    let classifier_filepath : &str = match classifier_filepath {
+        Some(filepath) => filepath,
+        None => LBP_CLASSIFIER_FILEPATH,
+    };
 
     let greyscaled_face_image = match imread(image_filepath, IMREAD_GRAYSCALE) {
         Ok(img) => img,
-        Err(_) => panic!("could not read the specified image"),
-    };
-    let mut colored_face_image = match imread(image_filepath, IMREAD_COLOR) {
-        Ok(img) => img,
-        Err(_) => panic!("could not read the specified image"),
+        Err(_) => return Err(()),
     };
 
-    let mut cascade_detector = CascadeClassifier::new(classifier_filepath).unwrap();
+    let mut cascade_detector = match CascadeClassifier::new(classifier_filepath) {
+        Ok(cascade) => cascade,
+        Err(_) => return Err(()),
+    };
 
+    // found these by trial an error, might try and up the scale factor if it's too slow
     let mut faces_detected : Vector<Rect> = Vector::new();
-    let scale_factor : f64 = 1.2;
+    let scale_factor : f64 = 1.25;
     let min_neighbours : i32 = 5;
     let flags : i32 = 0;
     let min_size : Size = Size::new(0, 0);
     let max_size : Size = Size::new(0, 0);
 
-    println!("running the facial detection with the following settings :\n   *scale_factor : {}\n   *min_neighbours : {}\n   *flags : {}\n   *min_size : {}\n   *max_size : {}",
-        scale_factor, min_neighbours, flags, min_size.area(), max_size.area()
-    );
+    if verbose {
+        println!("running the facial detection with the following settings :\n   *scale_factor : {}\n   *min_neighbours : {}\n   *flags : {}\n   *min_size : {}\n   *max_size : {}",
+            scale_factor, min_neighbours, flags, min_size.area(), max_size.area()
+        );
+    }
 
-    let now : Instant  = Instant::now();
+    match cascade_detector.detect_multi_scale(&greyscaled_face_image, &mut faces_detected, scale_factor, min_neighbours, flags, min_size, max_size) {
+        Ok(_) => (),
+        Err(_) => return Err(()),
+    }
 
-    cascade_detector.detect_multi_scale(&greyscaled_face_image, &mut faces_detected, scale_factor, min_neighbours, flags, min_size, max_size).unwrap();
-
-    println!("detected {} faces in {} ms", faces_detected.len(), now.elapsed().as_millis());
+    if verbose {
+        println!("number of faces detected : {}", faces_detected.len());
+    }
     
+    return Ok(faces_detected);
+}
 
-    let mut fdclone = faces_detected.clone();
-    for face in fdclone {
-        rectangle(&mut colored_face_image, face, Scalar::new(0.0, 0.0, 255.0, 0.0), 7,  LINE_8, 0).unwrap();
+pub fn draw_rectangle(image_filepath : &str, output_filepath : &str, faces : &Vector<Rect>) -> Result<(), ()> {
+
+    let mut image = match imread(image_filepath, IMREAD_COLOR) {
+        Ok(img) => img,
+        Err(_) => panic!("could not read the specified image"),
+    };
+
+    for face in faces.iter() {
+        let thickness : i32 = calculate_appropriate_rect_thickness(face.area());
+        match rectangle(&mut image, face, Scalar::new(0.0, 0.0, 255.0, 0.0), thickness,  LINE_8, 0) {
+            Ok(_) => (),
+            Err(_) => return Err(()),
+        }
     }
     let params : Vector<i32> = Vector::new();
-    imwrite("output.jpg", &colored_face_image, &params).unwrap();
-    return faces_detected;
-
+    match imwrite(output_filepath, &image, &params) {
+        // not sure ignoring the "false" bool result is wise but I didn't see anything about it on the openCV docs
+        Ok(_) => return Ok(()),
+        Err(_) => return Err(()),
+    }
 }
+
+fn calculate_appropriate_rect_thickness(area : i32) -> i32 {
+    let area : f64 = f64::from(area);
+    let side : f64 = area.sqrt();
+    let mut divided_side : f64 = side / 50.0;
+    divided_side = divided_side.ceil();
+    return divided_side as i32;
+}
+
 
 
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,5 @@
 mod facedetection;
 
 fn main() {
-    let class_filepath_0 : &str = "/home/ether/Shared/Warehouse/Rust/siberia/lbpcascade_frontalface_improved.xml";
-    let image_filepath : &str = "/home/ether/Shared/Warehouse/Rust/siberia/testing/doubleface.jpg";
-
-    facedetection::find_all_faces(image_filepath, class_filepath_0);
+    ()
 }
