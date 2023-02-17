fa1fd8ecd5fc26b4ba5cea2191e3e002daa0d861
anesthetice <elliot.currat@epfl.ch>
2023-02-17 21:19:39 +0100

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,5 @@ mod facedetection;
 mod facerecognition;
 
 fn main() {
-    /*
-    let faces = facedetection::find_all_faces("testing/img1.jpg", Some("haarcascade_frontalface_alt.xml"), None, true).unwrap();
-    facedetection::draw_rectangle("testing/img1.jpg", "output1.jpg", &faces);
 
-    let faces = facedetection::find_all_faces("testing/img2.jpg", Some("haarcascade_frontalface_alt.xml"), None, true).unwrap();
-    facedetection::draw_rectangle("testing/img2.jpg", "output2.jpg", &faces);
-
-    let faces = facedetection::find_all_faces("testing/img3.jpg", Some("haarcascade_frontalface_alt.xml"), None, true).unwrap();
-    facedetection::draw_rectangle("testing/img3.jpg", "output3.jpg", &faces);
-    */
 }
--- src/motioncapture.py
+++ src/motioncapture.py
@@ -34,10 +34,12 @@ currently disabled, see line 64
 # capture config for the still images that will be used for face detection and recognition
 capture_config = picam2.create_still_configuration()
 """
+
 # H264 seemingly doesn't work with 2304x1296, so I'll be using the standard 1080p 16:9 format
 video_config = picam2.create_video_configuration(main={"size": (1920, 1080), "format": "RGB888"},
                                                  lores={"size": lsize, "format": "YUV420"})
 picam2.configure(video_config)
+# high quality 1080p bitrate
 encoder = H264Encoder(162560000)
 picam2.encoder = encoder
 picam2.start()
