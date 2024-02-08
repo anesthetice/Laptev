e6f6c4cccfc803d24582a336002f2864ccba6e79
anesthetice <elliot.currat@epfl.ch>
2024-02-08 12:25:47 +0100

    working launch script, tested on rasp pi

--- laptev-host/launch.sh
+++ laptev-host/launch.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-# this script should be executed by .bashrc
+# this script should be executed on startup
 # launches the server and motion capture script
 # reboots after a day to prevent ugly bugs
 
-laptev="/home/$USER/Laptev"
+# i.e. copy the three lines bellow (uncomment after pasting them) in ~/.profile (or ~/.bash_login ~/.bash_profile for older systems)
+#if [ -f ~/Laptev/launch.sh ]; then
+#    if [ -z "$SSH_CONNECTION" ]; then
+#        nohup ~/Laptev/launch.sh &
+#    fi
+#fi
 
-nohup python "$laptev/motioncapture.py" > "$laptev/motioncapture.log"
-nohup "$laptev/laptev-host" > "$laptev/laptev-host.log"
+laptev="/home/$USER/Laptev"
+cd $laptev
+
+nohup python "$laptev/motioncapture.py" > "$laptev/motioncapture.log" &
+nohup "$laptev/laptev-host" > "$laptev/laptev-host.log" &
 
 sleep 86400
 reboot
