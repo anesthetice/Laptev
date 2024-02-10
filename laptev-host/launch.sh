5b6c19b7f34b7bda4a94fe4919c0f632428405ae
anesthetice <elliot.currat@epfl.ch>
2024-02-10 16:14:11 +0100

    launch update

--- laptev-host/launch.sh
+++ laptev-host/launch.sh
@@ -7,7 +7,11 @@
 # i.e. copy the three lines bellow (uncomment after pasting them) in ~/.profile (or ~/.bash_login ~/.bash_profile for older systems)
 #if [ -f ~/Laptev/launch.sh ]; then
 #    if [ -z "$SSH_CONNECTION" ]; then
-#        nohup ~/Laptev/launch.sh &
+#        if pgrep launch; then pkill launch; fi
+#        if pgrep sleep; then pkill sleep; fi
+#        if pgrep motioncapture; then pkill motioncapture; fi
+#        if pgrep laptev-host; then pkill laptev-host; fi
+#        nohup ~/Laptev/launch.sh > "/home/$USER/launch.log" 2>&1 &
 #    fi
 #fi
 
@@ -16,10 +20,7 @@ sleep 5
 laptev="/home/$USER/Laptev"
 cd $laptev
 
-if pgrep python; then pkill python; fi
-if pgrep laptev-host; then pkill laptev-host; fi
-
-nohup python "$laptev/motioncapture.py" > "$laptev/motioncapture.log" 2>&1 &
+nohup "$laptev/motioncapture.py" > "$laptev/motioncapture.log" 2>&1 &
 nohup "$laptev/laptev-host" > "$laptev/laptev-host.log" 2>&1 &
 
 sleep 86400
